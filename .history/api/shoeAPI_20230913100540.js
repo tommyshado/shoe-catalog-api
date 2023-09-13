bd1519b8bcb7725148d3db7c4608105a23cff3f4
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-09-13 10:55:24 +0200

    form is working

--- .history/api/shoeAPI_20230913100540.js
+++ .history/api/shoeAPI_20230913100540.js
@@ -17,10 +17,22 @@ export default function shoeAPI(shoeService) {
             res.status(500).json({ error: err.message });
         }
     }
+
+    async function addShoe(req, res) {
+        try {
+            const newShoe = req.body;
+            // Assuming shoeService.addShoe handles database insertion
+            await shoeService.addShoe(newShoe);
+            res.status(201).json({ message: 'Shoe added', shoe: newShoe });
+        } catch (error) {
+            res.status(500).json({ error: 'An error occurred' });
+        }
+    }
     
 
     return {
         getAll,
-        add
+        add,
+        addShoe
     }
 }
